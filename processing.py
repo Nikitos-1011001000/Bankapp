18faea38fa3027a4d25a6dbf807464f49b226176
Nikita Rukin <walfisch91@gmail.com>
2025-10-18 05:07:10 +0700

    правки от 13.10.25 11:59

--- processing.py
+++ processing.py
@@ -1,14 +1,9 @@
-def filter_by_state(records, state='EXECUTED'):
-    """
-    Фильтрует список словарей по значению ключа 'state'.
+def filter_by_state(records: int, state="EXECUTED"):
+    """ Фильтрует список словарей по значению ключа 'state'. """
+    return [record for record in records if record.get("state") == state]
 
-    :param records: список словарей
-    :param state: значение для фильтрации по ключу 'state' (по умолчанию 'EXECUTED')
-    :return: новый список словарей с нужным значением 'state'
-    """
-    return [record for record in records if record.get('state') == state]
 
-def sort_by_date(records, descending=True):
-    """принимает список словарей и необязательный параметр, задающий порядок сортировки(по умолчанию — убывание). возвращает новый список, отсортированный по дате"""
+def sort_by_date(records: int, descending=True):
+    """принимает список словарей и параметр, задающий порядок сортировки возвращает сортировку по дате"""
 
-    return sorted(records, key=lambda x: x.get('date'), reverse=descending)
+    return sorted(records, key=lambda x: x.get("date"), reverse=descending)
