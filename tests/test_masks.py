5a10c39f3c363c8895c7418c3bf9e07fdfa77ccc
Nikita Rukin <walfisch91@gmail.com>
2025-11-29 10:36:05 +0300

    29

--- tests/test_masks.py
+++ tests/test_masks.py
@@ -3,6 +3,7 @@ import pytest
 from bankapp.masks import get_mask_card_number
 from bankapp.masks import get_mask_account
 
+
 @pytest.fixture
 def card_numbers():
     return [
@@ -17,18 +18,17 @@ def test_get_mask_card_number(card_numbers):
     for card, expected in card_numbers:
         assert get_mask_card_number(card) == expected
 
-        @pytest.fixture
-        def account_numbers():
-            return [
-                # (входные данные, ожидаемый результат)
-                ("1234567890123456", "1234********3456"),  # обычный 16-значный номер
-                ("1234 5678 9012 3456", "1234********3456"),  # с пробелами, ожидается очистка
-                ("1234567", "1234567"),  # меньше ожидаемой длины, без изменений
-                ("", ""),  # пустая строка
-                ("1234567890", "1234**7890"),  # пример с 10 символами
-                ("ABCD1234EFGH5678", "ABCD********5678"),  # альфа-цифровой номер
-            ]
+@pytest.fixture
+def account_numbers():
+    return [
+        ("1234567890123456", "1234********3456"),  # обычный 16-значный номер
+        ("1234 5678 9012 3456", "1234********3456"),
+        ("1234567", "1234567"),  # меньше ожидаемой длины, без изменений
+        ("", ""),  # пустая строка
+        ("1234567890", "1234**7890"),  # пример с 10 символами
+        ("ABCD1234EFGH5678", "ABCD********5678"),  # альфа-цифровой номер
+    ]
 
-        def test_get_mask_account(account_numbers):
-            for account, expected in account_numbers:
-                assert get_mask_account(account) == expected
+def test_get_mask_account(account_numbers):
+    for account, expected in account_numbers:
+        assert get_mask_account(account) == expected
